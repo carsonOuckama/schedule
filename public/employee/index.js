616b0f4a3aad431e1b1b452513a6b2f3fdc2a153
Carson Ouckama <carsonouckama@gmail.com>
2024-07-03 12:54:29 -0600

    Employees can now see days taken off

--- public/employee/index.js
+++ public/employee/index.js
@@ -59,10 +59,25 @@ async function getSchedule() {
 }
 
 async function UpdateUserData() {
+    var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
     get("name").textContent = user.name;
     get("minimumDays").value = user.min;
     get("preferedDays").value = user.prefered;
     get("maximumDays").value = user.max;
+
+    var list = get("listOfDaysOff");
+    list.innerHTML = "";
+    for (var i = 1; i < user.daysOff.length; i++) {
+        var shiftDay = user.daysOff[i].split("|")[0];
+        var shiftType = user.daysOff[i].split("|")[1];
+
+        var obj = NumberToDay(parseInt(shiftDay));
+        var date = months[obj.month] + ", " + (obj.day+1);
+
+        let li = document.createElement('li');
+        li.textContent = date + ": " + (shiftType === "A" ? "All Day" : shiftType === "O" ? "Open" : shiftType === "C" ? "Close" : shiftType === "N" ? "Unavailable" : "ERR");
+        list.appendChild(li);
+    }
 }
 
 async function takeDayOff() {
@@ -84,20 +99,45 @@ async function takeDayOff() {
     if (response.ok) {
         get("dayOffMessage").textContent = "Complete!";
         get("dayOffMessage").style.color = "green";
+        await getInfo().then(UpdateUserData);
         return;
     } else if (response.status === 409) {
-        get("dayOffMessage").textContent = "This day is already taken off";
+        get("dayOffMessage").textContent = "This day is already up to date";
         get("dayOffMessage").style.color = "red";
     } else if (response.status === 404) {
         get("dayOffMessage").textContent = "Day not found!";
         get("dayOffMessage").style.color = "red";
     } else if (response.status === 403) {
-        get("dayOffMessage").textContent = "This day is not available to take off!";
+        get("dayOffMessage").textContent = "This shift is not available!";
         get("dayOffMessage").style.color = "red";
     }
 
     var data = await response.json();
     if (data.loggedIn === false) window.location.href = "/";
+}
+
+function NumberToDay(number) {
+    var year = 2024;
+    var leapYears = [2024, 2028, 2032, 2036, 2040, 2044, 2048, 2052];
+    
+    var expectedDays = leapYears.includes(year) ? 366 : 365;
+    while (number > expectedDays) {
+        year++;
+        number -= expectedDays;
+        expectedDays = leapYears.includes(year) ? 366 : 365;
+    }
+
+    var month = 0;
+    var months = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+    if (leapYears.includes(year)) {
+        months[1] = 29;
+    }
+    while (number > months[month]) {
+        number -= months[month];
+        month++;
+    }
+    return {month: month, day: number, year: year};
+    
 }
 
 getInfo().then(UpdateUserData);
